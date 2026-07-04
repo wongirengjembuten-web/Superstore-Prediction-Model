9c0b60165db775f96e9318a12e4bff8a8be16235
aiden <kuuhakurulezawarudo@gmail.com>
2026-07-04 14:36:47 +0700

    initial commit: Superstore

---
+++ Superstore.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import root_mean_squared_error
+
+
+
+
+def cleaning():
+    df = pd.read_csv("train.csv")
+    df = df.dropna()
+    df = df.drop_duplicates()
+    df["Ship Date"] = pd.to_datetime(df["Ship Date"],format="%d/%m/%Y")
+    df["Order Date"] = pd.to_datetime(df["Order Date"],format="%d/%m/%Y")
+
+    return df
+
+df = cleaning()
+
+# Preprocess
+Sales_per_day = df.groupby("Order Date")["Sales"].sum()
+print(Sales_per_day)
+
+rolling7 = Sales_per_day.rolling(window=7).mean()
+rolling30 = Sales_per_day.rolling(window=30).mean()
+seasonality = Sales_per_day - rolling30
+print(seasonality)
+
+plt.plot(Sales_per_day, alpha=0.3, label="Daily Sales (raw)")
+plt.plot(rolling7, label="7-Day Rolling Average")
+plt.plot(rolling30, label="30-Day Rolling Average", linewidth=2)
+
+plt.title("Sales Over Time — Raw vs Smoothed")
+plt.xlabel("Date")
+plt.ylabel("Sales")
+plt.legend()
+plt.show()
+
+
+
+df["Month label"] = df["Order Date"].dt.month
+df["date label"] = df["Order Date"].dt.day_of_week
+df["Year label"] = df["Order Date"].dt.year
+print(df["Month label"])
+print(df["date label"])
+print(df["Year label"])
+
+df = pd.merge(df,Sales_per_day,on="Order Date",how="inner")
+print(df.info())
+
+
+#training model
+training_data = df[df["Year label"] < 2018]
+test_data = df[df["Year label"] == 2018]
+print(training_data)
+print(len(test_data))
+
+
+X_train = training_data.filter(items=["Month label","date label","Year label"])
+y_train = training_data.filter(items=["Sales_y"])
+
+X_test = test_data.filter(items=["Month label","date label","Year label"])
+y_test = test_data.filter(items=["Sales_y"])
+
+model = RandomForestRegressor(n_estimators=200,random_state=42)
+
+model.fit(X_train,y_train)
+
+
+
+#Evaluate
+prediction = model.predict(X_test)
+
+print(prediction[:365])
+mae = mean_absolute_error(y_test, prediction)
+rmse = root_mean_squared_error(y_test, prediction)
+
+print("MAE :", mae)
+print("RMSE:", rmse)
+
+
+
+predicted_date = np.arange('2019-01-01', '2023-12-30', dtype='datetime64[D]')
+
+prediction_data = {"Prediction Date" : predicted_date,
+                 "Sales prediction" : prediction[:1824]
+                 }
+
+prediction_df = pd.DataFrame(prediction_data)
+
+
+print(len(predicted_date))
+print(len(prediction[:1824]))
+print(prediction_df.info())
+
+prediction_plot = prediction_df.groupby("Prediction Date")["Sales prediction"].sum()
+
+rolling7_p = prediction_plot.rolling(window=7).mean()
+rolling30_p= prediction_plot.rolling(window=30).mean()
+
+
+\
+plt.plot(prediction_plot,alpha=0.3,label="raw prediction")
+plt.plot(rolling7_p, label="7-Day Rolling Average(prediction)")
+plt.plot(rolling30_p, label="30-Day Rolling Average(prediction)", linewidth=2)
+plt.title("Sales Prediction")
+plt.xlabel("Date")
+plt.ylabel("Sales")
+plt.ylim(0,25000)
+plt.legend(loc="upper right")
+plt.show()
+
+
+
+
+
+
+
+
+
+#plt.plot(Sales_per_day, alpha=0.3, label="Daily Sales (raw)")
+#plt.plot(rolling7, label="7-Day Rolling Average")
+#plt.plot(rolling30, label="30-Day Rolling Average", linewidth=2)
+
+#plt.title("Sales Over Time — Raw vs Smoothed")
+#plt.xlabel("Date")
+#plt.ylabel("Sales")
+#plt.legend()
+
+
+
+
+
